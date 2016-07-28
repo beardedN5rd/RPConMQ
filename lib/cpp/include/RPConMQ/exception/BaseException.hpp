8f84d9eec08e299759f950725c18d450a1a66e53
Hagen Pache <hagen.pache@gateprotect.com>
2016-07-28 09:51:06 +0200

    corrected exception

--- lib/cpp/include/RPConMQ/exception/BaseException.hpp
+++ lib/cpp/include/RPConMQ/exception/BaseException.hpp
@@ -6,20 +6,13 @@ namespace RPConMQ {
 class BaseException
 :	public std::runtime_error
 {
-protected:
-    const std::string _message;
 public:
     explicit BaseException (const std::string& message)
-    :	_message(message)
+    :	runtime_error(message)
     {}
 
     virtual ~BaseException () throw ()
-	{}
-
-    virtual const char* what() const throw ()
-    {
-       return _message.c_str();
-    }
+    {} 
 };
 
 }
